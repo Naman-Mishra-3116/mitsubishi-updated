0d8beba68fd7d1b62c076da6e025b80ca5550c1f
Naman <namanmishra3116@gmail.com>
2025-05-09 21:38:10 +0530

    fixed all  the bugs

--- api/src/controllers/general.controllers/query/getCalendar.controller.ts
+++ api/src/controllers/general.controllers/query/getCalendar.controller.ts
@@ -8,28 +8,23 @@ export const getCalendar = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const info = await InfoModel.findOne({});
-    if (!info) {
-      return next(
-        new ErrorResponse(
-          ErrorType.BAD_REQUEST,
-          "Could not retreive calendar",
-          "This request could not be processed at this time"
-        )
-      );
-    }
-
-    console.log(info, "this is info");
-    return jsonResponse(res, {
-      status: "success",
-      statusCode: 200,
-      title: "Calendar fetched successfully",
-      data: {
-        calendarLink: info?.calenderLink,
-      },
-    });
-  } catch (error) {
-    console.log(error, "error is this");
+  const info = await InfoModel.findOne({});
+  if (!info) {
+    return next(
+      new ErrorResponse(
+        ErrorType.BAD_REQUEST,
+        "Could not retreive calendar",
+        "This request could not be processed at this time"
+      )
+    );
   }
+
+  return jsonResponse(res, {
+    status: "success",
+    statusCode: 200,
+    title: "Calendar fetched successfully",
+    data: {
+      calendarLink: info?.calenderLink,
+    },
+  });
 };
